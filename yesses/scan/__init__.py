3827976841c9c780879a1eb0efa28b738e9b9969
Daniel Fett <fett@danielfett.de>
2019-09-07 19:24:07 +0200

    New TLS settings scanner.

--- yesses/scan/__init__.py
+++ yesses/scan/__init__.py
@@ -1,4 +1,5 @@
 from .ports import Ports
 from .webservers import Webservers
 from .tls_settings import TLSSettings
+from .tls_settings_qualys import TLSSettingsQualys
 from .websecuritysettings import WebSecuritySettings
