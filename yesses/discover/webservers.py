b18b174d11e35a801b8f00f4099f471742d3986a
Fabian Hauck <hauckfabian@gmail.com>
2019-10-22 01:44:33 +0200

    added web server check on chosen and open ports

--- yesses/discover/webservers.py
+++ yesses/discover/webservers.py
@@ -7,27 +7,36 @@ from yesses.module import YModule, YExample
 log = logging.getLogger('scan/webservers')
 
 
-
 class Webservers(YModule):
     """Scans an IP range for web servers (on standard HTTP and HTTPs
 ports); combines a list of IPs with a list of domains to use for the
 Host header in web requests.
 
     """
-    
+
+    PORTS = [{"port": 80}, {"port": 443}]
+
     INPUTS = {
         "ips": {
             "required_keys": [
-                "ip"
+                "ip",
+                "port"
             ],
-            "description": "IP range to scan (e.g., `use HTTP-IPs and HTTPS-IPs`)",
-            "unwrap": True,
+            "description": "IPs and ports to scan (e.g. from the Ports module: Host-Ports)",
         },
         "domains": {
             "required_keys": [
                 "domain"
             ],
             "description": "Domain names to try on these IPs",
+            "unwrap": True,
+        },
+        "ports": {
+            "required_keys": [
+                "port"
+            ],
+            "description": "Ports to look for web servers",
+            "default": PORTS,
             "unwrap": True,
         }
     }
@@ -62,10 +71,19 @@ Host header in web requests.
   - discover Webservers:
       ips: 
         - ip: '93.184.216.34'
+          port: 80
+        - ip: '93.184.216.34'
+          port: 443
         - ip: '2606:2800:220:1:248:1893:25c8:1946'
+          port: 80
+        - ip: '2606:2800:220:1:248:1893:25c8:1946'
+          port: 443
       domains:
         - domain: example.com
         - domain: dev.example.com
+      ports:
+        - port: 80
+        - port: 443
     find:
       - Insecure-Origins
       - Secure-Origins
@@ -80,22 +98,28 @@ Host header in web requests.
         for ip in self.ips:
             for domain in self.domains:
                 for protocol in ('http', 'https'):
-                    with force_ip_connection(domain, ip):
-                        url = f'{protocol}://{domain}/'
-                        try:
-                            result = requests.get(url, timeout=10)
-                        except requests.exceptions.RequestException as e:
-                            log.debug(f"Exception {e} on {url}, ip={ip}")
-                        else:
-                            el = {'url': url, 'domain': domain, 'ip': ip}
-                            if protocol == 'https':
-                                output_secure.append(el)
-                                domain = {'domain': domain}
-                                if not domain in tls_domains:
-                                    tls_domains.append(domain)
+                    # just check a port if it is open and in the list of passed ports
+                    if ip['port'] in self.ports:
+                        with force_ip_connection(domain, ip['ip']):
+                            url = f"{protocol}://{domain}:{ip['port']}/"
+                            try:
+                                result = requests.get(url, timeout=10)
+                                # Don't add it as a web server if it just redirects the url
+                                # or doesn't respond with 200
+                                if url not in result.url or result.status_code != 200:
+                                    raise requests.exceptions.RequestException()
+                            except requests.exceptions.RequestException as e:
+                                log.debug(f"Exception {e} on {url}, ip={ip['ip']}")
                             else:
-                                output_insecure.append(el)
-                            log.info(f"Found webserver {url} on {ip}")
+                                el = {'url': url, 'domain': domain, 'ip': ip['ip']}
+                                if protocol == 'https':
+                                    output_secure.append(el)
+                                    dom = {'domain': domain}
+                                    if not dom in tls_domains:
+                                        tls_domains.append(dom)
+                                else:
+                                    output_insecure.append(el)
+                                log.info(f"Found webserver {url} on {ip['ip']}")
         self.results['Insecure-Origins'] = output_insecure
         self.results['Secure-Origins'] = output_secure
         self.results['TLS-Domains'] = tls_domains
