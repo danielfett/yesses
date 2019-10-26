59b1bf069903bd9779de4de66da9ba9f02eb1ed2
Fabian Hauck <hauckfabian@gmail.com>
2019-10-26 17:11:56 +0200

    search only in comments and visible text for information leakages in the information_leakage module; add requirements to requirement.txt

--- yesses/scan/information_leakage.py
+++ yesses/scan/information_leakage.py
@@ -1,11 +1,22 @@
 from typing import List
 import re
 import logging
+from bs4 import BeautifulSoup, Comment, ResultSet, Tag
+from comment_parser import comment_parser
 
 from yesses.module import YModule, YExample
 from yesses import utils
 
 log = logging.getLogger('scan/information_leakage')
+
+
+class InformationLeakageSession:
+
+    def __init__(self, soup: BeautifulSoup, page, dir_list: List[str], file_endings_list: List[str]):
+        self.soup = soup
+        self.page = page
+        self.dir_list = dir_list
+        self.file_endings_list = file_endings_list
 
 
 class InformationLeakage(YModule):
@@ -20,10 +31,10 @@ class InformationLeakage(YModule):
     """
 
     REGEX_IDENTIFIER = ["email", "ip", "path", "file"]
-    REGEX = [r"(^|\s)[a-zA-Z0-9-._]+@[a-zA-Z0-9-_]+\.[a-zA-Z0-9-]+(\s|$)",
-             r"(^|\s)([0-9]{1,3}\.){3}[0-9]{1,3}(\s|$)",
-             r"(^|\s)/([a-zA-Z0-9-_.]+/)*[a-zA-Z0-9-_.]+/?(\s|$)",
-             r"(^|\s)/?[a-zA-Z0-9-_]+\.[a-zA-Z0-9]+(\s|$)"]
+    REGEX = [r"[a-zA-Z0-9-._]+@[a-zA-Z0-9-_]+\.[a-zA-Z0-9-]+",
+             r"([0-9]{1,3}\.){3}[0-9]{1,3}(\s|$)",
+             r"/([a-zA-Z0-9-_.]+/)*[a-zA-Z0-9-_.]+/?",
+             r"/?[a-zA-Z0-9-_]+\.[a-zA-Z0-9]+"]
 
     DIR_LIST = "assets/information_leakage/common-directories.txt"
     FILE_ENDINGS_LIST = "assets/information_leakage/common-file-endings.txt"
@@ -52,8 +63,8 @@ class InformationLeakage(YModule):
         "Leakages": {
             "provided_keys": [
                 "url",
-                "line",
                 "type",
+                "found",
                 "finding"
             ],
             "description": "Potential information leakages"
@@ -65,11 +76,11 @@ class InformationLeakage(YModule):
       - scan Information Leakage:
           pages: 
             - url: page0
-              data: "\n\n test@example.com \n\n192.168.2.123test\n\n"
+              data: "<!-- test@example.com /var/home/bla --><html>\n\n<head><script src='ajkldfjalk'></script></head>\n\n <body>\n\n<!-- This is a comment --><h1>Title</h1>\n\n<!-- secret.txt \n\n/1x23/ex234--><p>Text with path /home/user/secret/key.pub</p> <a href='/docs/'>Website</a> <label>192.168.2.196 /usr/share/docs/ajdlkf/adjfl</label>\n\n<style> test@example.com </style>\n\n</body>"
             - url: page1
-              data: "/1x23/ex234\n\n /var/www/html \n\n /home/user/.profile\n\n /docs/"
+              data: "<html><script>// This is a js comment 192.168.170.128\n\nfunction {return 'Hello World';}\n\n</script><body></body><script>// Comment two \n\n console.log('test')/* Comment over\n\n several lines\n\n*/</script></html>"
             - url: page2
-              data: "\n\n 192.168.2.12 /usr/share/docs/ajdlkf/adjfl \n\n secret.txt"
+              data: "/*! modernizr 3.6.0 (Custom Build) | MIT *\n\n* https://modernizr.com/download/?-svgclippaths-setclasses !*/ \n\n!function(e,n,s){function o(e) // Comment three\n\n{var n=f.className,s=Modernizr._con /* Last \n\n multi \n\n line \n\n comment */ flakjdlfjldjfl"
         find:
           - Leakages
     """)
@@ -89,17 +100,62 @@ class InformationLeakage(YModule):
             return
 
         for page in self.pages:
-            lines = page['data'].split('\n')
-            for i, line in enumerate(lines):
-                for j, regex in enumerate(self.REGEX):
-                    matches = re.finditer(regex, line)
-                    for match in matches:
-                        if (j != 2 and j != 3) or self.check_file_or_path(match.group(0), dir_list, file_endings_list):
-                            log.debug(
-                                f"URL: {page['url']} Line: {i} Finding: {self.REGEX_IDENTIFIER[j]} => {match.group(0)}")
-                            self.results['Leakages'].append(
-                                {'url': page['url'], 'line': i, 'type': self.REGEX_IDENTIFIER[j],
-                                 'finding': match.group(0)})
+            soup = BeautifulSoup(page['data'], 'html.parser')
+
+            sess = InformationLeakageSession(soup, page, dir_list, file_endings_list)
+
+            # search in JavaScript comments for information leakage
+            self.check_js_css_comments(sess)
+
+            # search in the visible text for information leakages
+            self.check_visible_text(sess)
+
+            # search in comments for information leakages
+            self.check_html_comments(sess)
+
+    def check_visible_text(self, sess: InformationLeakageSession):
+        html = sess.soup.find_all('html')
+        if html:
+            for script in sess.soup(["script", "style"]):
+                script.extract()
+            text = sess.soup.get_text()
+            self.search_string(text, "visible_text", sess)
+
+    def check_html_comments(self, sess: InformationLeakageSession):
+        html = sess.soup.find_all('html')
+        if html:
+            comments = comment_parser.extract_comments_from_str(sess.page['data'], "text/html")
+            # comments = sess.soup.find_all(string=lambda text: isinstance(text, Comment))
+            for comment in comments:
+                self.search_string(comment._text, "comment", sess)
+
+    def check_js_css_comments(self, sess: InformationLeakageSession):
+        js_text = sess.soup.find_all('script')  # type: List[ResultSet]
+        html = sess.soup.find_all('html')
+        data = ''
+        if html and js_text:
+            for tag in js_text:  # type: Tag
+                data += tag.get_text() + '\n'
+        elif not html:
+            data = sess.page['data']
+
+        data = sess.page['data']
+
+        comments = comment_parser.extract_comments_from_str(data, "application/javascript")
+        for comment in comments:
+            self.search_string(comment._text, "css_js_comment", sess)
+
+    def search_string(self, text: str, found: str, sess: InformationLeakageSession):
+        for j, regex in enumerate(self.REGEX):
+            matches = re.finditer(regex, text)
+            for match in matches:
+                if (j != 2 and j != 3) or self.check_file_or_path(match.group(0), sess.dir_list,
+                                                                  sess.file_endings_list):
+                    log.debug(
+                        f"URL: {sess.page['url']} Finding: {self.REGEX_IDENTIFIER[j]} => {match.group(0)}")
+                    self.results['Leakages'].append(
+                        {'url': sess.page['url'], 'type': self.REGEX_IDENTIFIER[j],
+                         'found': found, 'finding': match.group(0)})
 
     def check_file_or_path(self, potential_path: str, dir_list: List[str], file_endings_list: List[str]) -> bool:
         if potential_path.split('.')[-1] in file_endings_list:
