99ac465d57d1d0037867b15c00123518fb3a4463
Fabian Hauck <hauckfabian@gmail.com>
2019-10-27 19:20:14 +0100

    check header for to much information

--- yesses/scan/__init__.py
+++ yesses/scan/__init__.py
@@ -3,3 +3,4 @@ from .tls_settings import TLSSettings
 from .tls_settings_qualys import TLSSettingsQualys
 from .websecuritysettings import WebSecuritySettings
 from .information_leakage import InformationLeakage
+from .header_leakage import HeaderLeakage
