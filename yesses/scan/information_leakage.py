c562f523f2f10ff34062b049c4e35983d28bbcbf
Fabian Hauck <hauckfabian@gmail.com>
2019-11-09 23:26:13 +0100

    added support for custom regular expressions; check ip address strings if numbers are in range; extract script and style strings from web pages to search them separately

--- yesses/scan/information_leakage.py
+++ yesses/scan/information_leakage.py
@@ -31,13 +31,12 @@ class InformationLeakage(YModule):
     which have a whitespace before or after.
     """
 
-    REGEX_IDENTIFIER = ["email", "ip", "path", "file", "server-info", "version-info"]
-    REGEX = [r"(^|\s)[a-zA-Z0-9-._]+@[a-zA-Z0-9-_]+\.[a-zA-Z0-9-]+(\s|$)",
-             r"([0-9]{1,3}\.){3}[0-9]{1,3}",
-             r"(^|\s)/?([a-zA-Z0-9-_.]+/)+[a-zA-Z0-9-_.]+/?(\s|$)",
-             r"(^|\s)/?[a-zA-Z0-9-_]+\.[a-zA-Z0-9]+(\s|$)",
-             r"(^|\s)[a-zA-Z_-]+/[0-9\.]+(\s\([a-zA-Z_-]+\))?(\s|$)",
-             r"(^|\s)[a-zA-Z0-9-_.]*[Vv]ersion:?\s([0-9]+\.)+[0-9]+"]
+    REGEX = {"email": r"(^|\s|\()[a-zA-Z0-9-._]+@[a-zA-Z0-9-_]+\.[a-zA-Z0-9-]+(\s|$|\))",
+             "ip": r"([0-9]{1,3}\.){3}[0-9]{1,3}",
+             "path": r"(^|\s|\()/?([a-zA-Z0-9-_.]+/)+[a-zA-Z0-9-_.]+/?(\s|$|\))",
+             "file": r"(^|\s|\()/?[a-zA-Z0-9-_]+\.[a-zA-Z0-9]+(\s|$|\))",
+             "server-info": r"(^|\s|\()[a-zA-Z_-]+/[0-9\.]+(\s\([a-zA-Z_-]+\))?(\s|$|\))",
+             "version-info": r"(^|\s|\()[a-zA-Z0-9-_.]*[Vv]ersion:?\s([0-9]+\.)+[0-9]+(\s|$|\))"}
 
     DIR_LIST = "assets/information_leakage/common-directories.txt"
     FILE_ENDINGS_LIST = "assets/information_leakage/common-file-endings.txt"
@@ -49,6 +48,14 @@ class InformationLeakage(YModule):
                 "data"
             ],
             "description": "Required. Pages to search for information leakage",
+        },
+        "search_regex": {
+            "required_keys": [
+                "type",
+                "regex"
+            ],
+            "description": "Own regular expression to search in pages (will be added to the existing ones)",
+            "default": {}
         },
         "dir_list": {
             "required_keys": None,
@@ -79,11 +86,14 @@ class InformationLeakage(YModule):
       - scan Information Leakage:
           pages: 
             - url: page0
-              data: "<!-- test@example.com /var/home/bla --><html>\n\n<head><script src='ajkldfjalk'></script></head>\n\n <body>\n\n<!-- This is a comment --><h1>Title</h1>\n\n<!-- secret.txt \n\n/1x23/ex234--><p>Text with path /home/user/secret/key.pub</p> <a href='/docs/'>Website</a> <label>192.168.2.196 /usr/share/docs/ajdlkf/adjfl</label>\n\n<style> test@example.com </style>\n\n</body>"
+              data: "<!-- test@example.com /var/home/bla aaa --><html>\n\n<head><script src='ajkldfjalk'></script></head>\n\n <body>\n\n<!-- This is a comment --><h1>Title</h1>\n\n<!-- secret.txt \n\n/1x23/ex234--><p>Text with path /home/user/secret/key.pub</p> <a href='/docs/'>Website</a> <label>192.168.2.196 /usr/share/docs/ajdlkf/adjfl</label>\n\n<style> test@example.com </style>\n\n</body>"
             - url: page1
-              data: "<html><script>// This is a js comment192.168.170.128\n\nfunction {return 'Hello World';}\n\n</script><body><p>bla Gitea Version: 1.11.0+dev-180-gd5b1e6bc5</p></body><script>// Comment two with email@example.com \n\n console.log('test')/* Comment over\n\n several lines\n\n*/</script></html>\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+              data: "<html><script>// This is a js comment192.256.170.128\n\nfunction {return 'Hello World';}\n\n</script><body><p>bla Gitea Version: 1.11.0+dev-180-gd5b1e6bc5</p></body><script>// Comment two with email@example.com \n\n console.log('test')/* Comment over\n\n several lines\n\n*/</script></html>\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
             - url: page2
               data: "/*! modernizr 3.6.0 (Custom Build) | MIT *\n\n* https://modernizr.com/download/?-svgclippaths-setclasses !*/ \n\n!function(e,n,s){function o(e) // Comment three\n\n{var n=f.className,s=Modernizr._con /* Last \n\n multi \n\n line \n\n comment */ flakjdlfjldjfl\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+          search_regex:
+            - type: new_regex
+              regex: (^|\s)a{3}(\s|$)
         find:
           - Leakages
     """)
@@ -102,6 +112,10 @@ class InformationLeakage(YModule):
             log.error("Could not open file endings list")
             return
 
+        # add custom regular expressions if there are any
+        for sr in self.search_regex:
+            self.REGEX[sr['type']] = sr['regex']
+
         for page in self.pages:
             soup = BeautifulSoup(page['data'], 'html.parser')
 
@@ -118,36 +132,59 @@ class InformationLeakage(YModule):
 
     def check_visible_text(self, sess: InformationLeakageSession):
         html = sess.soup.find_all('html')
-        if html:
-            for script in sess.soup(["script", "style"]):
-                script.extract()
-            text = sess.soup.get_text()
-            self.search_string(text, "visible_text", [1, 2, 3, 4, 5], sess)
+        if not html:
+            return
+
+        for script in sess.soup(["script", "style"]):
+            script.extract()
+        text = sess.soup.get_text()
+        self.search_string(text, "visible_text", ["email"], sess)
 
     def check_html_comments(self, sess: InformationLeakageSession):
-        comments = comment_parser.extract_comments_from_str(sess.page['data'], "text/html")
-        for comment in comments:
-            self.search_string(comment._text, "html_comment", list(range(len(self.REGEX))), sess)
+        html = sess.soup.find_all('html')
+        if not html:
+            return
+
+        for script in sess.soup(["script", "style"]):
+            script.extract()
+        text = sess.soup.get_text()
+        self.search_comments(sess.soup.get_text(), "html_comment", "text/html", sess)
 
     def check_js_css_comments(self, sess: InformationLeakageSession):
-        comments = comment_parser.extract_comments_from_str(sess.page['data'], "application/javascript")
-        for comment in comments:
-            self.search_string(comment._text, "css_js_comment", list(range(len(self.REGEX))), sess)
+        html = sess.soup.find_all('html')
+        # if there is no html tag then it is most likely a css or js file
+        if not html:
+            self.search_comments(sess.page['data'], "css_js_comment", "application/javascript", sess)
+        else:
+            # If there is an html tag then extract the script and style tags
+            # and search them.
+            for script in sess.soup(["script", "style"]):
+                self.search_comments(script.text, "css_js_comment", "application/javascript", sess)
 
-    def search_string(self, text: str, found: str, regex_list: List[int], sess: InformationLeakageSession):
-        for j in regex_list:
-            matches = re.finditer(self.REGEX[j], text)
+    def search_comments(self, text: str, type: str, mime: str, sess: InformationLeakageSession):
+        comments = comment_parser.extract_comments_from_str(text, mime)
+        for comment in comments:
+            self.search_string(comment._text, type, [], sess)
+
+    def search_string(self, text: str, found: str, no_search: List[str], sess: InformationLeakageSession):
+        for type, regex in self.REGEX.items():
+            if type in no_search:
+                continue
+            matches = re.finditer(regex, text)
             for match in matches:
                 finding = match.group(0).strip()
-                if (j != 2 and j != 3) or self.check_file_or_path(finding, sess.dir_list,
-                                                                  sess.file_endings_list):
-                    log.debug(
-                        f"URL: {sess.page['url']} Found: {found} Finding: {self.REGEX_IDENTIFIER[j]} => {finding}")
-                    self.results['Leakages'].append(
-                        {'url': sess.page['url'], 'type': self.REGEX_IDENTIFIER[j],
-                         'found': found, 'finding': finding})
+                if (type == 'path' or type == 'file') and not self.check_file_or_path(finding, sess.dir_list,
+                                                                                      sess.file_endings_list):
+                    continue
+                elif type == 'ip' and not self.check_ip_address(finding):
+                    continue
+                log.debug(
+                    f"URL: {sess.page['url']} Found: {found} Finding: {type} => {finding}")
+                self.results['Leakages'].append(
+                    {'url': sess.page['url'], 'type': type, 'found': found, 'finding': finding})
 
-    def check_file_or_path(self, potential_path: str, dir_list: List[str], file_endings_list: List[str]) -> bool:
+    @staticmethod
+    def check_file_or_path(potential_path: str, dir_list: List[str], file_endings_list: List[str]) -> bool:
         if potential_path.split('.')[-1] in file_endings_list:
             return True
 
@@ -165,6 +202,14 @@ class InformationLeakage(YModule):
         if i / len(split) >= 0.5:
             return True
         return False
+
+    @staticmethod
+    def check_ip_address(ip: str) -> bool:
+        splits = ip.split('.')
+        for split in splits:
+            if int(split) > 255:
+                return False
+        return True
 
 
 if __name__ == "__main__":
