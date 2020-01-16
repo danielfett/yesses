0a351098bca1a0a837c62d892351057451f19f6c
Daniel Fett <danielf@yes.com>
2020-01-16 15:41:18 +0100

    Improve output template.

--- yesses/outputs/template.py
+++ yesses/outputs/template.py
@@ -12,11 +12,13 @@ class Template:
         self.filename = Path(datetime.now().strftime(filename))
         self.template = Path(template)
 
-    def run(self, alertslist, steps, time):
+    def run(self, alertslist, steps, raw_config, time):
         file_loader = FileSystemLoader(str(self.template.parent))
         env = Environment(loader=file_loader)
         template = env.get_template(self.template.name)
-        output = template.render(time=time, steps=steps, **alertslist.get_vars())
+        output = template.render(
+            time=time, steps=steps, raw_config=raw_config, **alertslist.get_vars()
+        )
 
         self.filename.write_text(output)
         log.info(f"Wrote report to {self.filename} using template {self.template}")
