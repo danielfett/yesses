3de6c1c20ee4c89b151012c281eeaa3d9a43f1a2
Fabian Hauck <hauckfabian@gmail.com>
2020-03-26 16:25:13 +0100

    add subjectAltName extension to certificates

--- tests/certificates/create_certs.sh
+++ tests/certificates/create_certs.sh
@@ -15,6 +15,9 @@ openssl ecparam -out modern_key.pem -name prime256v1 -genkey
 openssl req -new -key modern_key.pem -out modern_cert.csr -sha512 \
   -subj "/C=TE/ST=TEST/L=TEST/O=TEST/CN=laravel.dev.intranet"
 openssl x509 -req -in modern_cert.csr -CA ecdsa_ca_cert.pem -CAkey ecdsa_ca_key.pem \
+  -extensions SAN \
+  -extfile <(cat /etc/ssl/openssl.cnf \
+    <(printf "\n[SAN]\nsubjectAltName=DNS:laravel.dev.intranet")) \
   -CAcreateserial -out modern_cert.pem -days 90 -sha512
 
 # Certificate for the nginx server
@@ -22,4 +25,9 @@ openssl genrsa -out intermediate_key.pem 2048
 openssl req -new -key intermediate_key.pem -out intermediate_cert.csr -sha256 \
   -subj "/C=TE/ST=TEST/L=TEST/O=TEST/CN=nginx.dev.intranet"
 openssl x509 -req -in intermediate_cert.csr -CA ecdsa_ca_cert.pem -CAkey ecdsa_ca_key.pem \
+  -extensions SAN \
+  -extfile <(cat /etc/ssl/openssl.cnf \
+    <(printf "\n[SAN]\nsubjectAltName=DNS:nginx.dev.intranet")) \
   -CAcreateserial -out intermediate_cert.pem -days 13 -sha256
+
+c_rehash .
